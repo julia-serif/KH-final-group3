40794282a17f8722b3d5d3fa2d2a7456d80048a3
jangEunsue <masterthd56641221@gmail.com>
2022-06-03 10:14:07 +0900

    PageDTO 추가

--- src/main/java/com/khie/model/MusicDTO.java
+++ src/main/java/com/khie/model/MusicDTO.java
@@ -1,5 +1,19 @@
 package com.khie.model;
 
 public class MusicDTO {
+	
+	private int m_no;
+    private String m_name;
+    private String m_cont;
+    private String m_artist;
+    private int m_like;
+    private String m_audio;
+    private String m_mv;
+    private String m_image;
+    private int m_pcount;
+    private int m_ptime;
+    private String m_lyrics;
+    private String m_date;
+    private String m_update;
 
 }
