a203d3808a25f69d05b38dfad5ddbfb23da39a20
julia-serif <juliainserif@gmail.com>
2022-06-15 14:21:16 +0900

    music_user table 추가, 그에 따른 reply 테이블들 변경. boolean 타입 없는 것에 대한 수정

--- sql/sql.sql
+++ sql/sql.sql
@@ -16,12 +16,25 @@ create table music (
     m_update date
 );
 
+create table music_user (
+    user_no number primary key,
+    user_id varchar2(50) unique not null,
+    user_pwd varchar2(100) not null,
+    user_name varchar(20) not null,
+    user_gender varchar2(20),
+    user_email varchar2(100) not null,
+    user_phone varchar2(30),
+    user_purchase char(5) check(user_purchase in('false', 'true')),
+    user_regdate date,
+    user_update date
+);
+
 -- 음원별 댓글 테이블
 create table m_reply (
     m_no number references music(m_no),
     mr_no number primary key,
     mr_cont varchar2(100) not null,
-    mr_writer varchar2(10) references user(user_id),
+    mr_writer varchar2(10) references music_user(user_id),
     mr_date date,
     mr_update date,
     mr_group number default 0,
@@ -33,7 +46,7 @@ create table v_reply (
     v_no number references music(m_no),
     vr_no number primary key,
     vr_cont varchar2(100) not null,
-    vr_writer varchar2(10) references user(user_id),
+    vr_writer varchar2(10) references music_user(user_id),
     vr_date date,
     vr_update date,
     vr_group number default 0,
@@ -71,7 +84,7 @@ create table music_playlist (
 	m_order number not null,		-- 플레이리스트 내 순서
 	playlist_order number not null,		-- 플레이리스트 순서
 	playlist_no number not null,		-- 플레이리스트 번호
-	playlist_name varchar2(1000) not null	-- 플레이리스트 이름
+	playlist_name varchar2(1000) not null,	-- 플레이리스트 이름
 	playlist_thumbnail varchar2(50)		-- 플레이리스트 섬네일
 );
 
@@ -80,6 +93,7 @@ create table music_by_user (
 	user_no number not null,	-- 회원 번호
 	m_no number not null,		-- 곡 번호
 	play_date date,			-- 플레이리스트 내 순서
-	play_thumbs boolean,		-- '좋아요' 여부
-	play_counts number		-- 곡 플레이 횟수
+	play_thumbs  varchar2(5) check(play_thumbs in('false', 'true')),		-- '좋아요' 여부
+	play_counts number,		-- 곡 플레이 횟수
+	constraint music_by_user_PK primary key(user_no, m_no)
 );
