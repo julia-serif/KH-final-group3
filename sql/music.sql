12da3d0d61362c4da8060840a850a15c90b7d4ed
julia-serif <juliainserif@gmail.com>
2022-06-24 13:44:58 +0900

    music.sql에 신규 sql 추가, sql의 music 테이블의 m_mv null 가능으로 변경

--- sql/music.sql
+++ sql/music.sql
@@ -60,3 +60,131 @@ It’s neither man nor woman
 Man nor woman
 It’s neither man nor woman
 (Just me loving Tomboy)', sysdate, '');
+
+
+insert into music values(2, 'POLAROID', '', 'I NEVER DIE', '(여자)아이들', default,
+'dummy-audio.mp3', '', 'Q_80,0.jpg', default, '219', 
+'철없는 우릴 사진에 담아서
+잊지 않게 간직하고 싶어 난
+In my pocket in my pocket
+
+A lot of people want us to grow up
+시간은 참 빠르게만 가는데
+I want pleasure want pleasure
+
+그땐 왜 모든 게 다 그저 어려웠을까
+Don’t be sad
+Let’s forget everything with the flash
+
+Can I take out the Polaroid
+And then we smile brightly
+슬퍼지더라도 울지는 않을게
+Let’s make a funny face
+
+Take out the Polaroid
+We shouldn’t forget this, please remember
+우리 사진 속에 그때 그 미소에
+Let’s make a funny face
+
+Ah ah ah ah ah ah
+
+Sometimes I think we can look back
+Cuz we’ve been too busy we’ve been too busy
+잠깐 쉬어가자 우리
+행복할 수 있게 더 웃을 수 있게
+
+그땐 왜 모든 게 다 그저 어려웠을까
+Don’t be sad
+Let’s forget everything with the flash
+
+Can I take out the Polaroid
+And then we smile brightly
+슬퍼지더라도 울지는 않을게
+Let’s make a funny face
+
+Take out the Polaroid
+We shouldn’t forget this, please remember
+우리 사진 속에 그때 그 미소에
+Let’s make a funny face
+
+Ah ah ah
+Polaroid, we should keep this memory
+Ah ah ah
+Take out the Polaroid
+
+Ah ah ah
+Polaroid, we should keep this memory
+Ah ah ah
+
+Can I take out the Polaroid
+And then we smile brightly
+슬퍼지더라도 울지는 않을게
+Let’s make a funny face
+
+Take out the Polaroid
+We shouldn’t forget this, please remember
+우리 사진 속에 그때 그 미소에
+Let’s make a funny face
+
+Ah ah ah
+Polaroid, we should keep this memory
+Ah ah ah
+Take out the Polaroid
+Ah ah ah ah ah ah
+
+Let’s make a funny face', sysdate, '');
+
+
+insert into music values(3, 'LOVE DIVE', '', 'LOVE DIVE', 'IVE (아이브)', default,
+'dummy-audio.mp3', 'https://www.youtube.com/embed/Y8JFxS1HlDo', 'Q_80,0 (1).jpg', default, '177', 
+'네가 참 궁금해 그건 너도 마찬가지
+이거면 충분해 쫓고 쫓는 이런 놀이
+참을 수 없는 이끌림과 호기심 
+묘한 너와 나 두고 보면 알겠지
+
+Woo 눈동자 아래로
+Woo 감추고 있는 거
+Woo yeah It’s so bad It’s good 
+난 그 맘을 좀 봐야겠어
+
+Narcissistic, my god I love it
+서로를 비춘 밤
+아름다운 까만 눈빛 더 빠져 깊이
+(넌 내게로 난 네게로) 
+숨 참고 love dive
+
+Woo lalalalalalala
+Woo 어서 와서 love dive
+Woo oh perfect sacrifice yeah
+숨 참고 love dive
+
+마음은 이렇게 알다가도 모르지
+사랑이라는 건 한순간에 필 테니
+직접 들어와 두 눈으로 확인해
+내 맘 가장 깊은 데로 오면 돼
+
+Woo 망설일 시간은
+Woo 3초면 되는 걸
+Woo yeah It’s so bad It’s good 
+원하면 감히 뛰어들어
+
+Narcissistic, my god I love it
+서로를 비춘 밤
+아름다운 까만 눈빛 더 빠져 깊이
+(넌 내게로 난 네게로) 
+숨 참고 love dive
+
+Woo lalalalalalala
+Woo 어서 와서 love dive
+Woo oh perfect sacrifice yeah
+숨 참고 love dive
+
+숨 참고 love dive 
+숨 참고 love dive
+숨 참고 love dive
+숨 참고 love dive
+
+Woo lalalalalalala
+Woo 어서 와서 love dive
+Woo oh perfect sacrifice yeah
+숨 참고 love dive', sysdate, '');
